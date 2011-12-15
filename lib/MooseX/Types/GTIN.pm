b3a2df94311fa3aed8834b8f4e1c86c23bb4e2e7
Dave Lambley <davel@state51.co.uk>
2011-12-15 12:13:00 +0000

    Spin up types.

--- lib/MooseX/Types/GTIN.pm
+++ lib/MooseX/Types/GTIN.pm
@@ -1,87 +1,40 @@
 package MooseX::Types::GTIN;
-
-use 5.010001;
-use strict;
-use warnings;
-
-require Exporter;
-use AutoLoader qw(AUTOLOAD);
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use MooseX::Types::GTIN ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 our $VERSION = '0.01';
 
+use 5.10;
+use MooseX::Types::Moose qw/Int Str/;
+use MooseX::Types::Structured qw(Dict);
+use MooseX::Types
+    -declare => [qw(
+        Barcode
+        ISBN10
+    )];
 
-# Preloaded methods go here.
+# import builtin types
+use Moose::Util::TypeConstraints;
+use Mooses::Types::GTIN::Validate;
+use Try::Tiny;
 
-# Autoload methods go after =cut, and are processed by the autosplit program.
+subtype Barcode, as Int,
+    where { try { MooseX::Types::GTIN::Validate::assert_barcode($_); 1; } },
+    message { local $@; eval { MooseX::Types::GTIN::Validate::assert_barcode($_); }; my $error = $@; $error =~ / at.+/; $error };
+
+coerce Barcode, from Int, via { "0$_" };
+
+subtype ISBN10, as Int,
+    where { length($_) == 10 };
+
+foreach my $type (Barcode, ISBN10) {
+    coerce $type, from Str, via { # Just trim all whitespace
+        s!\s+!!g; $_;
+    };
+}
+
+coerce Barcode, from ISBN10, via {
+    $_ = '978'.substr($_, 0, -1); # Prepend 978, Throw away last digit
+    $_ .= state51::Validate::calc_mod10_check_digit($_); # Add checksum
+    return $_;
+};
 
 1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
 
-=head1 NAME
-
-MooseX::Types::GTIN - Perl extension for blah blah blah
-
-=head1 SYNOPSIS
-
-  use MooseX::Types::GTIN;
-  blah blah blah
-
-=head1 DESCRIPTION
-
-Stub documentation for MooseX::Types::GTIN, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
-
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
-=head1 AUTHOR
-
-Dave Lambley, E<lt>davel@E<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2011 by Dave Lambley
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.10.1 or,
-at your option, any later version of Perl 5 you may have available.
-
-
-=cut
